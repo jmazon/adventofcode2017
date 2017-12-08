d1845bfdd12a99f715619f424bae8004392c565f
Jean-Baptiste Mazon <jb@darkstar>
2017-12-08 10:00:50 +0100

    Day 8

---
+++ level8.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import           Data.List        (scanl')
+import           Text.Regex.Posix ((=~),getAllTextSubmatches)
+import           Control.Arrow    ((&&&))
+import qualified Data.Map.Strict as M
+
+main = interact $ format . scanl' solve M.empty . parse
+
+parse = map f . lines where
+  f l = ( (ir,(readArith io (read ii)))
+        , (cr,(readRel   co (read ci))) ) where
+    [ir,io,ii,cr,co,ci] = tail $ getAllTextSubmatches $
+      l =~ "([a-z]+) (inc|dec) (-?[0-9]+) if ([a-z]+) ([<>=!]+) (-?[0-9]+)"
+  readArith "inc" = (+)
+  readArith "dec" = subtract
+  readRel ">"  = (< ) -- I'm blatantly sparing a `flip` here too ^^
+  readRel ">=" = (<=)
+  readRel "==" = (==)
+  readRel "!=" = (/=)
+  readRel "<"  = (> )
+  readRel "<=" = (>=)
+        
+solve m (instr,cond) | evalOp m cond = M.insert (fst instr) (evalOp m instr) m
+                     | otherwise = m
+  where evalOp m (r,o) = o (maybe 0 id (M.lookup r m))
+
+format = show . (last &&& maximum) . map maximum . dropWhile M.null
