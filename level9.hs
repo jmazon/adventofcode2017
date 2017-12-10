faf0a75eb14ee5ebbcfa3669ab288043b06098cd
Jean-Baptiste Mazon <jb@darkstar>
2017-12-11 00:45:37 +0100

    Day 9

---
+++ level9.hs
@@ -0,0 +1,10 @@
+import Control.Arrow ((***))
+import Text.Parsec
+main = interact $ show . parse (group 1) "source"
+group d = char '{' *> groupElement d <* char '}'
+groupElement d = ((d+) . sum *** sum) . unzip <$>
+                 sepBy (group (d+1) <|> garbage) (char ',')
+garbage = char '<' *> garbageElement <* char '>'
+garbageElement = ((,) 0) . sum <$>
+                 many ( (char '!' *> anyChar *> pure 0) <|>
+                        (noneOf ">" *> pure 1) )
