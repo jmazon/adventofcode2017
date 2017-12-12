b0020393ada29d572ef45244b407a4f5501553ef
Jean-Baptiste Mazon <jb@yavin>
2017-12-12 11:42:11 +0100

    Day 12

---
+++ level12.hs
@@ -0,0 +1,11 @@
+import Data.Char     (isDigit)
+import Data.List     (groupBy)
+import Data.Function (on)
+import Data.Graph    (buildG,reachable,scc)
+import Control.Arrow ((&&&))
+
+main = interact $ show . (length . flip reachable 0 &&& length . scc) .
+                  uncurry buildG . (((,) 0) . pred . length &&& concat) .
+                  map ( uncurry map . ((,) . head &&& tail) . map read .
+                        filter (isDigit . head) . groupBy ((==) `on` isDigit) ) .
+                  lines
